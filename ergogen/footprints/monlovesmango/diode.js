418ebe1b8e82011c6f973b6ce669f529ccec9ddb
monlovesmango <monlovesmango@users.noreply.github.com>
2024-10-15 18:34:42 -0500

    remove diode from front

--- ergogen/footprints/monlovesmango/diode.js
+++ ergogen/footprints/monlovesmango/diode.js
@@ -19,13 +19,6 @@ module.exports = {
       (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
       
       ${''/* diode symbols */}
-      (fp_line (start 0.25 0) (end 0.75 0) (layer F.SilkS) (width 0.1))
-      (fp_line (start 0.25 0.4) (end -0.35 0) (layer F.SilkS) (width 0.1))
-      (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer F.SilkS) (width 0.1))
-      (fp_line (start -0.35 0) (end 0.25 -0.4) (layer F.SilkS) (width 0.1))
-      (fp_line (start -0.35 0) (end -0.35 0.55) (layer F.SilkS) (width 0.1))
-      (fp_line (start -0.35 0) (end -0.35 -0.55) (layer F.SilkS) (width 0.1))
-      (fp_line (start -0.75 0) (end -0.35 0) (layer F.SilkS) (width 0.1))
       (fp_line (start 0.25 0) (end 0.75 0) (layer B.SilkS) (width 0.1))
       (fp_line (start 0.25 0.4) (end -0.35 0) (layer B.SilkS) (width 0.1))
       (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer B.SilkS) (width 0.1))
@@ -35,10 +28,8 @@ module.exports = {
       (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))
   
       ${''/* SMD pads on both sides */}
-      (pad 1 smd rect (at -1.65 0 ${p.r}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.to})
       (pad 2 smd rect (at 1.65 0 ${p.r}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.from})
       (pad 1 smd rect (at -1.65 0 ${p.r}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.to})
-      (pad 2 smd rect (at 1.65 0 ${p.r}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.from})
       
       ${''/* THT terminals */}
       (pad 1 thru_hole rect (at -3.81 0 ${p.r}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.to})
