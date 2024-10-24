2e06e013bab75a22b3cfecc0c7b81d5099169e34
monlovesmango <monlovesmango@users.noreply.github.com>
2024-10-23 21:57:15 -0500

    add pcb hole footprint

--- ergogen/footprints/monlovesmango/pcb_hole.js
+++ ergogen/footprints/monlovesmango/pcb_hole.js
@@ -1,12 +1,13 @@
 module.exports = {
   params: {
-    class: 'HOLE'
+    class: 'HOLE',
+    diam: 0
   },
   body: p => `
     (module HOLE_M2_TH (layer F.Cu) (tedit 5A52E4A3) 
       ${p.at /* parametric position */}   
       ${'' /* footprint reference */}
 
-      (pad "" np_thru_hole circle (at 0 0 90) (size 5.4 5.4) (drill 5.4) (layers *.Cu *.Mask B.SilkS) (clearance 0.3)))
+      (pad "" np_thru_hole circle (at 0 0 90) (size ${p.diam} ${p.diam}) (drill ${p.diam}) (layers *.Cu *.Mask B.SilkS) (clearance 0.3)))
   `
 }
